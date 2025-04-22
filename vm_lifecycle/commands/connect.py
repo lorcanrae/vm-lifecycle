2f6c642531d40c3f9a6cc62e11221eb6580191bf
Lorcan Rae <lorcanrae@gmail.com>
2025-04-22 18:27:56 +0300

    connect.py: added connection logic, checks, and flag for custom path

--- vm_lifecycle/commands/connect.py
+++ vm_lifecycle/commands/connect.py
@@ -1,42 +1,46 @@
 import click
 import subprocess
-from vm_lifecycle.utils import load_config
-
-# TODO: all of this
-
-
-def describe_vm(zone: str, project: str) -> str:
-    """Get the description of the VM"""
-    response = str(
-        subprocess.check_output(
-            [
-                "gcloud",
-                "compute",
-                "instances",
-                "describe",
-                f"--zone={zone}",
-                "--project={project}",
-            ]
-        )
-    )
-    return response
+from vm_lifecycle.utils import (
+    load_config,
+    describe_vm,
+    check_running,
+    pre_run_checks,
+    create_vscode_ssh,
+)
+from vm_lifecycle.params import TF_WORKSPACES
 
 
-def check_running(status) -> bool:
-    return "RUNNING" in status
-
-
-@click.command()
-def connect():
+@click.command(name="connect")
+@click.option(
+    "--path", type=click.Path(), default="/home/", help="Path to Open VS Code on VM"
+)
+def vscode_connect(path):
     "Connect to VM in VS Code inside ~/code/<username>"
-    config = load_config(__file__)
-    if not check_running(describe_vm(config["zone"], config["project_id"])):
-        print("VM is stopped. Run 'vmlc start' before trying to connect")
+    for workspace in TF_WORKSPACES:
+        check = pre_run_checks(workspace)
+
+    if not check:
+        return
+
+    # Build connection alias with gcloud
+    config = load_config()
+
+    create_vscode_ssh()
+
+    # Make sure the VM is on
+    if not check_running(
+        describe_vm(config["zone"], config["project_id"], config["instance_name"])
+    ):
+        print(
+            "VM is stopped. Run 'vmlc create' or 'vmlc restore' before trying to connect"
+        )
     else:
+        vscode_path = path if path else f"/home/{config['instance_user']}/code/"
+
         subprocess.run(
             [
                 "code",
                 "--folder-uri",
-                f"vscode-remote://ssh-remote+{config['instance_name']}.{config['zone']}.{config['project_id']}/home/{config['instance_user']}/code/",
+                f"vscode-remote://ssh-remote+{config['instance_name']}.{config['zone']}.{config['project_id']}{vscode_path}",
             ]
         )
