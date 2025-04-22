52e591a8c94d7f3c3e504494dbd3a4646e1a13f3
Lorcan Rae <lorcanrae@gmail.com>
2025-04-22 17:49:26 +0300

    created central params file

---
+++ vm_lifecycle/params.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+##### Paths
+
+
+ROOT_DIR = Path(__file__).parent.parent
+DEFAULT_CONFIG_PATH = ROOT_DIR / "config.yaml"
+
+##### Misc lists
+
+TF_WORKSPACES = ["vm-create", "vm-archive", "vm-restore"]
+
+GCP_MACHINE_TYPES = [
+    "e2-micro",
+    "e2-small",
+    "e2-medium",
+    "e2-standard-2",
+    "e2-standard-4",
+    "n1-standard-1",
+    "n1-standard-2",
+    "n2-standard-2",
+    "n2-standard-4",
+]
+
+
+# if __name__ == "__main__":
+#     print(ROOT_DIR)
+#     print(DEFAULT_CONFIG_PATH)
