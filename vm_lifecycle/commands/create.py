838fae5751c156eba906d4b71c6b548488700617
Lorcan Rae <lorcanrae@gmail.com>
2025-04-22 18:26:22 +0300

    create.py: global vars

--- vm_lifecycle/commands/create.py
+++ vm_lifecycle/commands/create.py
@@ -1,6 +1,7 @@
 import subprocess
 import click
-from vm_lifecycle.utils import get_root_dir, pre_run_checks
+from vm_lifecycle.utils import pre_run_checks
+from vm_lifecycle.params import ROOT_DIR
 
 
 @click.command(name="create")
@@ -8,5 +9,4 @@ def create_vm():
     """Create GCP VM using Terraform"""
     pre_run_checks(workspace="vm-create")
     click.echo("Creating VM...")
-    root = get_root_dir(__file__)
-    subprocess.run(["make", "apply", "workspace=vm-create"], check=True, cwd=root)
+    subprocess.run(["make", "apply", "workspace=vm-create"], check=True, cwd=ROOT_DIR)
