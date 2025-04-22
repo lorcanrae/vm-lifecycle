871a5eabeeed8e60e8eeae98eafed4681032e31f
Lorcan Rae <lorcanrae@gmail.com>
2025-04-22 18:28:40 +0300

    main.py: added vscode_connect fn

--- vm_lifecycle/main.py
+++ vm_lifecycle/main.py
@@ -4,6 +4,7 @@ from vm_lifecycle.commands.archive import archive_vm
 from vm_lifecycle.commands.restore import restore_vm_from_image
 from vm_lifecycle.commands.destroy import destroy_all
 from vm_lifecycle.commands.config import init
+from vm_lifecycle.commands.connect import vscode_connect
 
 
 @click.group()
@@ -17,7 +18,7 @@ cli.add_command(archive_vm)
 cli.add_command(restore_vm_from_image)
 cli.add_command(init)
 cli.add_command(destroy_all)
-
+cli.add_command(vscode_connect)
 
 if __name__ == "__main__":
     cli()
