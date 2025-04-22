66f92007b6584fb6b5d673175bcf530a5ef0b920
Lorcan Rae <lorcanrae@gmail.com>
2025-04-22 15:19:20 +0300

    added input config init input validation for instance name, project id, and machine type

--- vm_lifecycle/utils.py
+++ vm_lifecycle/utils.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 import yaml
 import click
+import re
 
 DEFAULT_CONFIG_PATH = Path(__file__).parent.parent / "config.yaml"
+
+######## Config
 
 
 def load_config(path: Path = DEFAULT_CONFIG_PATH) -> dict:
     if not path.exists():
         click.echo(
-            f"Config file `config.yaml` not found at: {path}. Run `devm init config` to create config.yaml"
+            f"Config file 'config.yaml' not found at: {path}. Run 'devm init config' to create config.yaml"
         )
         return {}
 
@@ -28,27 +31,9 @@ def write_tfvars_from_config(config: dict, workspace: str):
             f.write(f'{key} = "{value}"\n')
 
 
-def pre_run_checks(workspace: str) -> bool:
-    # Compare config with .tfvars
-    config = load_config(DEFAULT_CONFIG_PATH)
-
-    validate_tfvars_with_config(config=config, workspace=workspace)
-
-    # Check if .terraform/ and .terraform.lock.hcl exists in workspace
-    workspace_dir = Path(__file__).parent.parent / "infra" / workspace
-    file_check = [".terraform", ".terraform.lock.hcl"]
-    for file in file_check:
-        if workspace_dir / file not in workspace_dir.iterdir():
-            click.echo(
-                f"{file} not found in {workspace}. Run `devm init tf --{workspace}` or `devm init tf`"
-            )
-            return False
-    return True
-
-
 def validate_tfvars_with_config(config: dict, workspace: str) -> bool:
     """
-    Compare values in `config.yaml` with `terraform.tfvars` file in a given workspace.
+    Compare values in 'config.yaml' with 'terraform.tfvars' file in a given workspace.
 
     Returns:
     - True: if match
@@ -59,7 +44,7 @@ def validate_tfvars_with_config(config: dict, workspace: str) -> bool:
 
     if not tfvars_path.exists():
         click.echo(f"❌ tfvars file not found at {tfvars_path}.")
-        click.echo(f"Run `vmlc init tf --workspace={workspace}` or `vmlc init tf`")
+        click.echo(f"Run 'vmlc init tf --workspace={workspace}' or 'vmlc init tf'")
         return False
 
     with tfvars_path.open("r") as f:
@@ -91,6 +76,61 @@ def validate_tfvars_with_config(config: dict, workspace: str) -> bool:
         return False
 
     return True
+
+
+######## Input Validation
+
+GCP_MACHINE_TYPES = [
+    "e2-micro",
+    "e2-small",
+    "e2-medium",
+    "e2-standard-2",
+    "e2-standard-4",
+    "n1-standard-1",
+    "n1-standard-2",
+    "n2-standard-2",
+    "n2-standard-4",
+]
+
+
+def is_valid_project_id(pid: str) -> bool:
+    return re.match(r"^[a-z][a-z0-9\-]{4,28}[a-z0-9]$", pid) is not None
+
+
+def is_valid_instance_name(name: str) -> bool:
+    return re.match(r"^[a-z]([-a-z0-9]{0,61}[a-z0-9])?$")
+
+
+def prompt_validation(prompt_text, validator_fn, error_msg):
+    while True:
+        value = click.prompt(prompt_text, type=str)
+        if validator_fn(value):
+            return value
+        click.echo(f"❌ {error_msg}\n")
+
+
+######## Pre-run
+
+
+def pre_run_checks(workspace: str) -> bool:
+    # Compare config with .tfvars
+    config = load_config(DEFAULT_CONFIG_PATH)
+
+    validate_tfvars_with_config(config=config, workspace=workspace)
+
+    # Check if .terraform/ and .terraform.lock.hcl exists in workspace
+    workspace_dir = Path(__file__).parent.parent / "infra" / workspace
+    file_check = [".terraform", ".terraform.lock.hcl"]
+    for file in file_check:
+        if workspace_dir / file not in workspace_dir.iterdir():
+            click.echo(
+                f"{file} not found in {workspace}. Run 'devm init tf --{workspace}' or 'devm init tf'"
+            )
+            return False
+    return True
+
+
+######## Misc
 
 
 def get_root_dir(file: Path):
