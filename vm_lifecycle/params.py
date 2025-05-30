2fdcfedf9251e53a180e3a20a4cda0cdf1856227
Lorcan Rae <lorcanrae@gmail.com>
2025-05-30 13:25:25 +0300

    refactored params.py, removed tf vars

--- vm_lifecycle/params.py
+++ vm_lifecycle/params.py
@@ -1,18 +1,11 @@
 from pathlib import Path
 
 ##### Paths
-
-
 ROOT_DIR = Path(__file__).parent.parent
 DEFAULT_CONFIG_PATH = ROOT_DIR / "config.yaml"
 
 ##### Misc lists
-
-TF_WORKSPACES = ["vm-create", "vm-archive", "vm-restore"]
-
 GCP_MACHINE_TYPES = [
-    "e2-micro",
-    "e2-small",
     "e2-medium",
     "e2-standard-2",
     "e2-standard-4",
