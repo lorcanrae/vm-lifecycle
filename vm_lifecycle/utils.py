cd5cf79e83f8c45aebb021d9c3f770f02a56bcdc
Lorcan Rae <lorcanrae@gmail.com>
2025-06-10 12:10:17 +0300

    moved list profiles from config_manager.py to utils.py

--- vm_lifecycle/utils.py
+++ vm_lifecycle/utils.py
@@ -1,32 +1,10 @@
-from pathlib import Path
-import yaml
 import click
 import re
 import subprocess
-from vm_lifecycle.params import DEFAULT_CONFIG_PATH
-
-
-######## Config
-# Superseded
-
-
-def load_config(path: Path = DEFAULT_CONFIG_PATH, warn: bool = True) -> dict:
-    if not path.exists():
-        if warn:
-            click.echo(
-                f"❗ 'config.yaml' not found at: {path}. Run 'vmlc config create' to create a profile in 'config.yaml'"
-            )
-        return {}
-
-    with path.open("r") as f:
-        config = yaml.safe_load(f)
-
-    return config
+from typing import List, Optional
 
 
 ######## Click Config Input Validation
-
-
 def is_valid_profile_name(profile_id: str) -> bool:
     return re.match(r"^[a-z][a-z0-9\-]{1,20}[a-z0-9]$", profile_id) is not None
 
@@ -47,9 +25,47 @@ def prompt_validation(prompt_text, validator_fn, error_msg):
         click.echo(f"❌ {error_msg}\n")
 
 
+def select_from_list(
+    profiles: List[str],
+    prompt_message: str,
+    confirm_message_fn: Optional[callable] = None,
+    confirm: bool = False,
+    default: Optional[int] = None,
+):
+    if not profiles:
+        click.echo("❌ No items available.")
+        return None
+
+    # List profiles
+    for i, p in enumerate(profiles, 1):
+        click.echo(f"{i}. {p}")
+    while True:
+        try:
+            default_display = default + 1 if default is not None else None
+            idx = click.prompt(prompt_message, type=int, default=default_display)
+            if 1 <= idx <= len(profiles):
+                selection = profiles[idx - 1]
+                if confirm:
+                    confirm_msg = (
+                        confirm_message_fn(selection)
+                        if confirm_message_fn
+                        else f"Are you sure you want to select '{selection}'?"
+                    )
+                    if not click.confirm(confirm_msg, default=False):
+                        click.echo("Aborted.")
+                        return None
+                return selection
+            else:
+                click.echo("❌ Invalid choice.")
+        except (click.exceptions.Abort, KeyboardInterrupt):
+            click.echo("\n❌ Aborted")
+            return
+        except Exception:
+            click.echo("❌ Invalid input.")
+    return None
+
+
 ######## VSCode
-
-
 def create_vscode_ssh():
     subprocess.run(["gcloud", "compute", "config-ssh"])
 
