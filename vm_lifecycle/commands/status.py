b8efe7017a17fc3c337c9548ca14b0e245b385bd
Lorcan Rae <lorcanrae@gmail.com>
2025-06-17 13:15:11 +0300

    commands/status.py: added command to list images

--- vm_lifecycle/commands/status.py
+++ vm_lifecycle/commands/status.py
@@ -6,19 +6,36 @@ from vm_lifecycle.gcp_helpers import init_gcp_context
 
 
 @click.command(name="status")
-def gcp_vm_instance_status():
+@click.option(
+    "-i", "--images", is_flag=True, help="Retrieve list of images for active project."
+)
+def gcp_vm_instance_status(images):
     """List GCP Compute Engine instance resources"""
 
     config_manager, compute_manager, active_zone = init_gcp_context()
     if not config_manager:
         sys.exit(1)
 
-    subprocess.run(
-        [
-            "gcloud",
-            "compute",
-            "instances",
-            "list",
-            f"--project={config_manager.active_profile['project_id']}",
-        ]
-    )
+    if not images:
+        subprocess.run(
+            [
+                "gcloud",
+                "compute",
+                "instances",
+                "list",
+                f"--project={config_manager.active_profile['project_id']}",
+            ]
+        )
+
+    if images:
+        found_images = compute_manager.list_images()
+
+        if found_images:
+            click.echo(
+                f"💿 Found {len(found_images)} image{'s' if len(found_images) > 1 else ''}:"
+            )
+            for image in found_images:
+                click.echo(f"\t{image['name']}")
+            sys.exit(0)
+        else:
+            sys.exit(1)
